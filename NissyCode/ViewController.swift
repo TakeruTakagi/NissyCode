693d81d287d1fb4b16f68a15e39576d6ef7e5e97
TakeruTakagi <tryte5@icloud.com>
2022-12-19 23:41:59 +0900

    first commit

---
+++ NissyCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NissyCode
+//
+//  Created by 髙木　武 on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
