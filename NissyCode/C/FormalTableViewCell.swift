27e9f536af92f831cbe886090f2a51b4d7bc08e5
TakeruTakagi <tryte5@icloud.com>
2023-01-16 00:24:06 +0900

    tableView表示

--- NissyCode/C/FormalTableViewCell.swift
+++ NissyCode/C/FormalTableViewCell.swift
@@ -8,26 +8,12 @@
 import UIKit
 import RealmSwift
 
+
+
 class FormalTableViewCell: UITableViewCell {
-    
-    var favoriteFlag: Bool = false
-    let apparel = ApparelDataModel()
-    
-    var realm: Realm!
     
     @IBOutlet weak var ApparelText: UILabel!
     @IBOutlet weak var starButton: UIButton!
-    
-    @IBAction func Like(_ sender: Any) {
-        apparel.starButton = true
-    }
-    
-    @IBAction func add(_ sender: Any) {
-        try! realm.write{
-            realm.add(apparel)
-        }
-    }
-    
     @IBOutlet weak var apparelImage: UIImageView!
     @IBOutlet weak var onepointText: UILabel!
     @IBOutlet weak var apparelLink: UILabel!
@@ -37,10 +23,10 @@ class FormalTableViewCell: UITableViewCell {
         // Initialization code
     }
     
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
         // Configure the view for the selected state
     }
-    
 }
--- NissyCode/C/FormalTableViewController.swift
+++ NissyCode/C/FormalTableViewController.swift
@@ -5,57 +5,50 @@
 //  Created by 髙木　武 on 2022/12/27.
 //
 
-import Foundation
 import UIKit
 import RealmSwift
 
-class FormalTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FormalTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+   
     
     @IBOutlet weak var FormalTableView: UITableView!
     
-    var realm: Realm!
     
-    var formalFassions: [ApparelDataModel] = []
+    var formalFassions: [ApparelDataModel] = [
+        ApparelDataModel(id: "1", apparelText: "ニット", apparelImage: "1", starButton: false, onePointText: "ゆるふわにっと", link: "http~")
+    ]
     
-    let apparel = ApparelDataModel()
+    
+    var apparel = ApparelDataModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FormalTableView.delegate = self
+        FormalTableView.dataSource = self
         FormalTableView.register(UINib(nibName: "FormalTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
+        FormalTableView.tableFooterView = UIView()
         
-        let formal1 = ApparelDataModel(id: "1", apparelText: "パーカー", apparelImage: "2002", starButton: false, onePointText: "オーバーサイズ", link: "http~~")
+        FormalTableView.estimatedRowHeight = 1000
+        FormalTableView.rowHeight = UITableView.automaticDimension
         
+        FormalTableView.reloadData()
         
-        let formalFassions = [formalFassions]
+        let formalFassions = formalFassions
+        self.formalFassions = formalFassions
         
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        realm = try! Realm()
-        apparel.starButton = false
-        print(apparel.starButton)
-    }
-    
-    
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return formalFassions.count
     }
-    
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 200
-        }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-        }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! FormalTableViewCell  //.xibカスタムセル使用時
         let apparelDetaModel :ApparelDataModel = formalFassions[formalFassions.count - indexPath.row - 1]
         
         cell.ApparelText.text = apparelDetaModel.apparelText
+        
         cell.apparelImage.image = UIImage(named: "")
         cell.onepointText.text = apparelDetaModel.onePointText
         cell.apparelLink.text = apparelDetaModel.link
